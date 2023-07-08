dbe31e6c71d5e92e26429ed325f942b8ab2a528c
Ibrahim Sobh <im_sobh@hotmail.com>
2023-07-08 13:55:50 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,8 +30,8 @@ def generate_response(uploaded_file, google_api_key, query_text):
         db = Chroma.from_documents(texts, embeddings) 
         
         # Create retriever interface
-        # retriever = db.as_retriever(k=3)
-        retriever = db.as_retriever(search_type="similarity_score_threshold", search_kwargs={"score_threshold": .9})
+        retriever = db.as_retriever(k=2, fetch_k=4)
+        # retriever = db.as_retriever(search_type="similarity_score_threshold", search_kwargs={"score_threshold": .9})
         
         # Create QA chain
         #qa = RetrievalQA.from_chain_type(llm=GooglePalm(google_api_key=google_api_key, temperature=0.1, max_output_tokens=128), chain_type="stuff", retriever=retriever)
