e280ae2dc7cdbccbbb0c015cf3cea22ad240a78a
Ibrahim Sobh <im_sobh@hotmail.com>
2023-07-08 10:40:40 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ def generate_response(uploaded_file, openai_api_key, query_text):
         
         # Select embeddings
         #embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
-        embeddings = GooglePalmEmbeddings()
+        embeddings = GooglePalmEmbeddings(google_api_key=openai_api_key)
         
         # Create a vectorstore from documents
         db = Chroma.from_documents(texts, embeddings) 
@@ -58,7 +58,8 @@ result = []
 with st.form('myform', clear_on_submit=True):
     openai_api_key = st.text_input('OpenAI API Key', type='password', disabled=not (uploaded_file and query_text))
     submitted = st.form_submit_button('Submit', disabled=not(uploaded_file and query_text))
-    if submitted and openai_api_key.startswith('AIz'):
+    #if submitted and openai_api_key.startswith('AIz'):
+    if submitted and openai_api_key:
         with st.spinner('Calculating...'):
             response = generate_response(uploaded_file, openai_api_key, query_text)
             result.append(response)
